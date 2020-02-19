b4ea352d81662d9b779fbdb6cc693411666cf45c
Lucas Siqueira <lucassiqzro@gmail.com>
2020-02-19 00:30:11 -0300

    Create global style

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: #7d40e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+`;
