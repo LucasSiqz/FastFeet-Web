cf955712adf548c9fc080d1639b336c623f12a33
Lucas Siqueira <lucassiqzro@gmail.com>
2020-03-08 23:18:48 -0300

    Update styles

--- src/components/DefaultAvatar/styles.js
+++ src/components/DefaultAvatar/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.span`
   border-radius: 50%;
   margin-right: 5px;
-  font-size: ${props => `${props.size / 3}px`};
+  font-size: ${props => `${props.size / 2}px`};
   width: ${props => `${props.size}px`};
   height: ${props => `${props.size}px`};
   background: ${props => props.background};
